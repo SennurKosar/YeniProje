6315f765b43df42f3046ef7e655d5e41807c28a1
SennurKosar <kosarsennur85@gmail.com>
2022-03-22 12:33:20 +0100

    yeni proje calismasi

---
+++ README.md
@@ -0,0 +1,5 @@
+#MERHABA
+
+##Grup1 Projemize hosgeldiniz
+
+###Super!...
---
+++ src/test/java/Day1/YeniProje.java
@@ -0,0 +1,7 @@
+package Day1;
+
+public class YeniProje {
+    public static void main(String[] args) {
+        // Süper Grup projesi
+    }
+}
